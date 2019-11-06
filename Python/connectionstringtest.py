811a3a30aa551450aa78d5253e080293f9d1ca5e
gvargas3 <gabevargas2010@gmail.com>
2019-11-06 11:51:33 -0500

    python dist change

--- Python/connectionstringtest.py
+++ Python/connectionstringtest.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import os
 import json
 
+
 def getConnections(c):
     #Some code to query what PI boards are available and gets their names
     connectionArray = ['Connection 1', 'Connection 2', 'Connection 3']
