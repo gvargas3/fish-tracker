f646c32db22e3f3f9acf15d3c02024e82527360b
brentmw1 <brent.wickenheiser@rockets.utoledo.edu>
2019-11-06 12:33:17 -0500

    added winwifi and connection scan
    scan + wifi

--- Python/connectionstringtest.py
+++ Python/connectionstringtest.py
@@ -1,10 +1,18 @@
 from __future__ import print_function
 import os
 import json
+import winwifi
 
 def getConnections(c):
     #Some code to query what PI boards are available and gets their names
-    connectionArray = ['Connection 1', 'Connection 2', 'Connection 3']
+    x = winwifi.WinWiFi()
+    netList = x.scan()
+    connectionArray=['test']
+    for p in netList:
+        connectionArray.append(p._ssid)
+    
+    #['Connection 1', 'Connection 2', 'Connection 3']
+    print(connectionArray)
     return  connectionArray
 
 def connect(self,c):
@@ -27,3 +35,4 @@ def getScreenshot(self):
 
     filepath = 'images/test.jpg'
     return filepath
+#getConnections(0)
